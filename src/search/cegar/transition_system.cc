4af02aa105dbca9e103d322cc2f5353abfd3ac03
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-01-27 14:48:08 +0100

    [cegar-ocp] add transition_system

---
+++ src/search/cegar/transition_system.cc
@@ -0,0 +1,22 @@
+#include "transition_system.h"
+
+#include <cassert>
+
+using namespace std;
+
+namespace cegar {
+TransitionSystem::TransitionSystem(const Abstraction &) {
+    /*
+    // Store transition system.
+    for (AbstractState *abstract_state : states) {
+        for (const Arc transition : abstract_state->get_outgoing_arcs()) {
+            OperatorProxy op = transition.first;
+            AbstractState *succ_state = transition.second;
+            operator_to_transitions[op.get_id()].push_back(num_transitions);
+            state_to_incoming_transitions[succ_state].push_back(num_transitions);
+            state_to_outgoing_transitions[abstract_state].push_back(num_transitions);
+            ++num_transitions;
+        }
+    }*/
+}
+}
---
+++ src/search/cegar/transition_system.h
@@ -0,0 +1,43 @@
+#ifndef CEGAR_TRANSITION_SYSTEM_H
+#define CEGAR_TRANSITION_SYSTEM_H
+
+#include <vector>
+
+class OperatorProxy;
+class State;
+
+namespace cegar {
+class Abstraction;
+
+struct Transition {
+    int start;
+    int op;
+    int end;
+
+    Transition(int s1_id, int op_id, int s2_id)
+      : start(s1_id), op(op_id), end(s2_id) {
+    }
+    ~Transition() = default;
+};
+
+class TransitionSystem {
+
+public:
+    explicit TransitionSystem(const Abstraction &abstraction);
+    ~TransitionSystem() = default;
+
+    int get_num_abstract_states() const;
+
+    int get_abstract_state_index(const State &concrete_state) const;
+
+    bool is_dead_end(const State &concrete_state) const;
+
+    bool induces_self_loop(const OperatorProxy &op) const;
+
+    const std::vector<int> &get_goal_indices() const;
+
+    const std::vector<Transition> &get_transitions() const;
+};
+}
+
+#endif
