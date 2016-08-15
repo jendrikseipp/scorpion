a959665dfd141949e93793f1116a7a438ed4cd21
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-08-15 11:28:09 +0200

    [cegar-ocp] fix state coyping in sampling function

--- src/search/sampling.cc
+++ src/search/sampling.cc
@@ -21,8 +21,6 @@ vector<State> sample_states_with_random_walks(
     const utils::CountdownTimer *timer) {
     vector<State> samples;
 
-    const State &initial_state = task_proxy.get_initial_state();
-
     int n;
     if (init_h == 0) {
         n = 10;
@@ -54,8 +52,8 @@ vector<State> sample_states_with_random_walks(
                 ++length;
         }
 
-        // Sample one state with a random walk of length length.
-        State current_state(initial_state);
+        // Sample one state with a random walk of length 'length'.
+        State current_state = task_proxy.get_initial_state();
         vector<OperatorProxy> applicable_ops;
         for (int j = 0; j < length; ++j) {
             applicable_ops.clear();
@@ -71,11 +69,11 @@ vector<State> sample_states_with_random_walks(
                 /* If current state is a dead end, then restart the random walk
                    with the initial state. */
                 if (is_dead_end(current_state))
-                    current_state = State(initial_state);
+                    current_state = task_proxy.get_initial_state();
             }
         }
         // The last state of the random walk is used as a sample.
-        samples.push_back(current_state);
+        samples.push_back(move(current_state));
     }
     return samples;
 }
