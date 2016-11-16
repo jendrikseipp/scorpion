b36239abe036ed083bc5b4a7744b196217c2a55c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-11-16 15:12:45 +0100

    [scp-pdbs] add cost_saturation::OptimalCostPartitioningHeuristic

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -423,7 +423,7 @@ fast_downward_plugin(
         cost_saturation/diversifier.cc
         cost_saturation/explicit_abstraction.cc
         cost_saturation/merge_and_shrink_abstraction_generator.cc
-        #cost_saturation/optimal_cost_partitioning_heuristic.cc
+        cost_saturation/optimal_cost_partitioning_heuristic.cc
         cost_saturation/projection.cc
         cost_saturation/projection_generator.cc
         cost_saturation/saturated_cost_partitioning_heuristic.cc
