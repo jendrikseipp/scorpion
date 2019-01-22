d8ab6478991bc92f0976ba0d510406e338ea856c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-01-22 11:59:12 +0100

    [scp-pattern-generator] Filtered systematic: remove options for keeping the n best PDBs.

--- src/search/pdbs/pattern_collection_generator_filtered_systematic.cc
+++ src/search/pdbs/pattern_collection_generator_filtered_systematic.cc
@@ -80,16 +80,6 @@ static PatternCollection get_patterns(
     return patterns;
 }
 
-template<class T, class S, class C>
-S &Container(priority_queue<T, S, C> &q) {
-    struct HackedQueue : private priority_queue<T, S, C> {
-        static S &Container(priority_queue<T, S, C> &q) {
-            return q.*& HackedQueue::c;
-        }
-    };
-    return HackedQueue::Container(q);
-}
-
 
 class SequentialPatternGenerator {
     shared_ptr<AbstractTask> task;
@@ -124,21 +114,6 @@ public:
 };
 
 
-struct Candidate {
-    unique_ptr<cost_saturation::Projection> projection;
-    double score;
-
-    Candidate(unique_ptr<cost_saturation::Projection> &&projection, double score)
-        : projection(move(projection)),
-          score(score) {
-    }
-
-    bool operator>(const Candidate &other) const {
-        return score > other.score;
-    }
-};
-
-
 PatternCollectionGeneratorFilteredSystematic::PatternCollectionGeneratorFilteredSystematic(
     const Options &opts)
     : max_pattern_size(opts.get<int>("max_pattern_size")),
@@ -146,55 +121,7 @@ PatternCollectionGeneratorFilteredSystematic::PatternCollectionGeneratorFiltered
       max_collection_size(opts.get<int>("max_collection_size")),
       max_patterns(opts.get<int>("max_patterns")),
       max_time(opts.get<double>("max_time")),
-      keep_best(opts.get<bool>("keep_best")),
-      scoring_function(static_cast<ScoringFunction>(opts.get_enum("scoring_function"))),
-      saturate(opts.get<bool>("saturate")),
       debug(opts.get<bool>("debug")) {
-}
-
-double PatternCollectionGeneratorFilteredSystematic::rate_projection(
-    const cost_saturation::Projection &projection,
-    const vector<int> &costs,
-    const State &initial_state) {
-    vector<int> h_values = projection.compute_goal_distances(costs);
-    vector<int> saturated_costs = projection.compute_saturated_costs(
-        h_values, costs.size());
-    double avg_h = compute_mean_finite_value(h_values);
-    int init_id = projection.get_abstract_state_id(initial_state);
-    double init_h = h_values[init_id];
-    int size = projection.get_num_states();
-    int used_costs = 0;
-    for (int c : saturated_costs) {
-        if (c > 0) {
-            used_costs += c;
-        }
-    }
-    used_costs = max(used_costs, 1);
-
-    double score;
-    if (scoring_function == ScoringFunction::INIT_H) {
-        score = init_h;
-    } else if (scoring_function == ScoringFunction::AVG_H) {
-        score = avg_h;
-    } else if (scoring_function == ScoringFunction::INIT_H_PER_COSTS) {
-        score = init_h / used_costs;
-    } else if (scoring_function == ScoringFunction::AVG_H_PER_COSTS) {
-        score = avg_h / used_costs;
-    } else if (scoring_function == ScoringFunction::INIT_H_PER_SIZE) {
-        score = init_h / size;
-    } else if (scoring_function == ScoringFunction::AVG_H_PER_SIZE) {
-        score = avg_h / size;
-    } else {
-        ABORT("Scoring function not implemented");
-    }
-
-    if (debug) {
-        cout << "pattern: " << projection.get_pattern() << ", init-h: "
-             << init_h << ", avg-h: " << avg_h << ", costs: " << used_costs
-             << ", size: " << size << endl;
-    }
-
-    return score;
 }
 
 PatternCollectionInformation
@@ -208,7 +135,7 @@ PatternCollectionGeneratorFilteredSystematic::select_systematic_patterns(
     vector<int> variable_domains = get_variable_domains(task_proxy);
     vector<int> costs = task_properties::get_operator_costs(task_proxy);
     SequentialPatternGenerator pattern_generator(task, max_pattern_size);
-    priority_queue<Candidate, vector<Candidate>, greater<vector<Candidate>::value_type>> candidates;
+    shared_ptr<ProjectionCollection> projections = make_shared<ProjectionCollection>();
     int64_t collection_size = 0;
     while (true) {
         if (timer.is_expired()) {
@@ -227,55 +154,34 @@ PatternCollectionGeneratorFilteredSystematic::select_systematic_patterns(
             continue;
         }
 
-        if (!keep_best && static_cast<int>(candidates.size()) == max_patterns) {
+        if (static_cast<int>(projections->size()) == max_patterns) {
             cout << "Reached maximum number of patterns." << endl;
             break;
         }
 
-        if (!keep_best &&
-            max_collection_size != numeric_limits<int>::max() &&
+        if (max_collection_size != numeric_limits<int>::max() &&
             pdb_size > static_cast<int64_t>(max_collection_size) - collection_size) {
             continue;
         }
 
         unique_ptr<cost_saturation::Projection> projection =
             utils::make_unique_ptr<cost_saturation::Projection>(task_proxy, task_info, pattern);
-        double score = 0.;
-        if (saturate) {
-            vector<int> goal_distances = projection->compute_goal_distances(costs);
-            score = compute_mean_finite_value(goal_distances);
-            if (score > 0.) {
-                vector<int> saturated_costs = projection->compute_saturated_costs(
-                    goal_distances, costs.size());
-                cost_saturation::reduce_costs(costs, saturated_costs);
-            }
-        } else {
-            score = rate_projection(*projection, costs, initial_state);
-        }
+        vector<int> goal_distances = projection->compute_goal_distances(costs);
+        double score = compute_mean_finite_value(goal_distances);
         if (score > 0.) {
             utils::Log() << "Add pattern " << projection->get_pattern()
                          << " with score " << score << endl;
-            candidates.emplace(move(projection), score);
+            vector<int> saturated_costs = projection->compute_saturated_costs(
+                goal_distances, costs.size());
+            cost_saturation::reduce_costs(costs, saturated_costs);
+            projections->push_back(move(projection));
             collection_size += pdb_size;
-        }
-
-        if (keep_best && static_cast<int>(candidates.size()) > max_patterns) {
-            if (debug) {
-                cout << "Remove pattern " << candidates.top().projection->get_pattern()
-                     << " with score " << candidates.top().score << endl;
-            }
-            collection_size -= candidates.top().projection->get_num_states();
-            candidates.pop();
         }
     }
     shared_ptr<PatternCollection> patterns = make_shared<PatternCollection>();
-    shared_ptr<ProjectionCollection> projections = make_shared<ProjectionCollection>();
-    patterns->reserve(candidates.size());
-    projections->reserve(candidates.size());
-    // The PQ doesn't allow retrieving the unique_ptrs.
-    for (Candidate &candidate : Container(candidates)) {
-        patterns->push_back(candidate.projection->get_pattern());
-        projections->push_back(move(candidate.projection));
+    patterns->reserve(projections->size());
+    for (auto &projection : *projections) {
+        patterns->push_back(projection->get_pattern());
     }
     PatternCollectionInformation pci(task_proxy, patterns);
     pci.set_projections(projections);
@@ -314,26 +220,6 @@ static void add_options(OptionParser &parser) {
         "maximum time in seconds for generating patterns",
         "infinity",
         Bounds("0.0", "infinity"));
-    parser.add_option<bool>(
-        "keep_best",
-        "keep best patterns",
-        "false");
-    vector<string> scoring_functions;
-    scoring_functions.push_back("INIT_H");
-    scoring_functions.push_back("AVG_H");
-    scoring_functions.push_back("INIT_H_PER_COSTS");
-    scoring_functions.push_back("AVG_H_PER_COSTS");
-    scoring_functions.push_back("INIT_H_PER_SIZE");
-    scoring_functions.push_back("AVG_H_PER_SIZE");
-    parser.add_enum_option(
-        "scoring_function",
-        scoring_functions,
-        "scoring function",
-        "INIT_H");
-    parser.add_option<bool>(
-        "saturate",
-        "compute saturated cost function after adding a pattern",
-        "false");
     parser.add_option<bool>(
         "debug",
         "print debugging messages",
