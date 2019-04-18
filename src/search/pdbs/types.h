fd43e5e9d0dbd171ebc9dada1903d188063ec7b8
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-04-18 10:08:07 +0200

    [scp-pattern-generator] Add typedef.

--- src/search/pdbs/types.h
+++ src/search/pdbs/types.h
@@ -1,6 +1,8 @@
 #ifndef PDBS_TYPES_H
 #define PDBS_TYPES_H
 
+#include "../utils/hash.h"
+
 #include <memory>
 #include <vector>
 
@@ -14,6 +16,7 @@ using Pattern = std::vector<int>;
 using PatternCollection = std::vector<Pattern>;
 using PDBCollection = std::vector<std::shared_ptr<PatternDatabase>>;
 using ProjectionCollection = std::vector<std::unique_ptr<cost_saturation::Projection>>;
+using PatternSet = utils::HashSet<Pattern>;
 using PatternID = int;
 /* NOTE: pattern cliques are often called maximal additive pattern subsets
    in the literature. A pattern clique is an additive set of patterns,
