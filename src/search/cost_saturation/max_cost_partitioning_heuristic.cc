d4e91aae9e3a0629d93b278f84bd3210a7611a70
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-03-24 13:56:01 +0100

    [scp-online] Log time in offline heuristic.

--- src/search/cost_saturation/max_cost_partitioning_heuristic.cc
+++ src/search/cost_saturation/max_cost_partitioning_heuristic.cc
@@ -86,6 +86,12 @@ MaxCostPartitioningHeuristic::MaxCostPartitioningHeuristic(
                  << num_abstractions << " = "
                  << static_cast<double>(num_useful_abstractions) / num_abstractions
                  << endl;
+
+    compute_heuristic_timer = utils::make_unique_ptr<utils::Timer>(false);
+    convert_global_state_timer = utils::make_unique_ptr<utils::Timer>(false);
+    get_abstract_state_ids_timer = utils::make_unique_ptr<utils::Timer>(false);
+    unsolvability_heuristic_timer = utils::make_unique_ptr<utils::Timer>(false);
+    compute_max_h_timer = utils::make_unique_ptr<utils::Timer>(false);
 }
 
 MaxCostPartitioningHeuristic::~MaxCostPartitioningHeuristic() {
@@ -93,17 +99,34 @@ MaxCostPartitioningHeuristic::~MaxCostPartitioningHeuristic() {
 }
 
 int MaxCostPartitioningHeuristic::compute_heuristic(const GlobalState &global_state) {
+    convert_global_state_timer->resume();
     State state = convert_global_state(global_state);
+    convert_global_state_timer->stop();
     return compute_heuristic(state);
 }
 
 int MaxCostPartitioningHeuristic::compute_heuristic(const State &state) const {
+    compute_heuristic_timer->resume();
+
+    get_abstract_state_ids_timer->resume();
     vector<int> abstract_state_ids = get_abstract_state_ids(
         abstraction_functions, state);
+    get_abstract_state_ids_timer->stop();
+
+    unsolvability_heuristic_timer->resume();
     if (unsolvability_heuristic.is_unsolvable(abstract_state_ids)) {
+        unsolvability_heuristic_timer->stop();
+        compute_heuristic_timer->stop();
         return DEAD_END;
     }
-    return compute_max_h_with_statistics(cp_heuristics, abstract_state_ids, num_best_order);
+    unsolvability_heuristic_timer->stop();
+
+    compute_max_h_timer->resume();
+    int max_h = compute_max_h_with_statistics(cp_heuristics, abstract_state_ids, num_best_order);
+    compute_max_h_timer->stop();
+
+    compute_heuristic_timer->stop();
+    return max_h;
 }
 
 void MaxCostPartitioningHeuristic::print_statistics() const {
@@ -114,5 +137,10 @@ void MaxCostPartitioningHeuristic::print_statistics() const {
          << num_best_order << endl;
     cout << "Probably useful orders: " << num_probably_useful << "/" << num_orders
          << " = " << 100. * num_probably_useful / num_orders << "%" << endl;
+    cout << "Time for computing heuristic: " << *compute_heuristic_timer << endl;
+    cout << "Time for converting state: " << *convert_global_state_timer << endl;
+    cout << "Time for computing abstract state IDs: " << *get_abstract_state_ids_timer << endl;
+    cout << "Time for checking unsolvability: " << *unsolvability_heuristic_timer << endl;
+    cout << "Time for computing max_h: " << *compute_max_h_timer << endl;
 }
 }
