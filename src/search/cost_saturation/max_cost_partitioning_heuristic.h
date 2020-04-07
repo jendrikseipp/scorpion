44f773931df428ae62197f1587d5ceee3b736eae
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-04-07 13:52:28 +0200

    [scp-online] Remove offline timers.

--- src/search/cost_saturation/max_cost_partitioning_heuristic.h
+++ src/search/cost_saturation/max_cost_partitioning_heuristic.h
@@ -13,10 +13,6 @@ namespace options {
 class Options;
 }
 
-namespace utils {
-class Timer;
-}
-
 namespace cost_saturation {
 class AbstractionFunction;
 class CostPartitioningHeuristic;
@@ -31,11 +27,6 @@ class MaxCostPartitioningHeuristic : public Heuristic {
 
     // For statistics.
     mutable std::vector<int> num_best_order;
-    std::unique_ptr<utils::Timer> compute_heuristic_timer;
-    std::unique_ptr<utils::Timer> convert_global_state_timer;
-    std::unique_ptr<utils::Timer> get_abstract_state_ids_timer;
-    std::unique_ptr<utils::Timer> unsolvability_heuristic_timer;
-    std::unique_ptr<utils::Timer> compute_max_h_timer;
 
     void print_statistics() const;
     int compute_heuristic(const State &state) const;
