80e49d36957d86b4616ff0180047eb18137523d1
Jendrik Seipp <jendrik.seipp@liu.se>
2021-04-20 12:54:29 +0200

    [scorpion] Add Scorpion build configs.

--- build_configs.py
+++ build_configs.py
@@ -4,5 +4,12 @@ debug = ["-DCMAKE_BUILD_TYPE=Debug"]
 glibcxx_debug = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO", "-DUSE_GLIBCXX_DEBUG=YES"]
 minimal = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_PLUGINS_BY_DEFAULT=YES"]
 
+_plugins = ["PLUGIN_ASTAR", "COST_SATURATION", "OPERATOR_COUNTING"]
+_project = ["-DDISABLE_PLUGINS_BY_DEFAULT=YES"] + [f"-DPLUGIN_{plugin}_ENABLED=YES" for plugin in _plugins]
+project = ["-DCMAKE_BUILD_TYPE=Release"] + _project
+projectdebug = ["-DCMAKE_BUILD_TYPE=Debug"] + _project
+del _plugins
+del _project
+
 DEFAULT = "release"
 DEBUG = "debug"
