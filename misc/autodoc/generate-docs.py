e13d2dbc394f7709cd34edc5f69efa169868000a
Jendrik Seipp <jendrik.seipp@liu.se>
2021-09-28 22:21:50 +0200

    [docs] Add internal links and fix nested lists.

--- misc/autodoc/generate-docs.py
+++ misc/autodoc/generate-docs.py
@@ -13,7 +13,6 @@ SCRIPT_DIR = Path(__file__).resolve().parent
 REPO_ROOT_DIR = SCRIPT_DIR.parents[1]
 
 TXT2TAGS_OPTIONS = {
-    "toc": False,
     "preproc": [
         [r"<<BR>>", "ESCAPED_LINEBREAK"],
     ],
@@ -37,18 +36,42 @@ def build_planner(build):
     subprocess.check_call([sys.executable, "build.py", build, "downward"], cwd=REPO_ROOT_DIR)
 
 
+def insert_wiki_links(text, titles):
+    def make_link(m):
+        anchor = m.group("anchor") or ""
+        link_name = m.group("link")
+        target = link_name + ".md"
+        if anchor:
+            target = link_name + ".md#" + anchor.lower()
+            link_name = anchor
+        link_name = link_name.replace("_", " ")
+        result = m.group("before") + f'[""{link_name}"" {target}]' + m.group("after")
+        return result
+
+    # List of characters allowed in doc titles. Matches 'word characters' including '_', '+', and '-'.
+    title_white_list = r"[\w\+-]"
+
+    re_link = r"(?P<before>\W)(?P<link>%s)(#(?P<anchor>" + title_white_list + r"+))?(?P<after>\W)"
+    for title in titles:
+        text = re.sub(re_link % title, make_link, text)
+    return text
+
+
 def build_docs(build, outdir):
     out = subprocess.check_output(
         ["./fast-downward.py", "--build", build, "--search", "--", "--help", "--txt2tags"],
         cwd=REPO_ROOT_DIR).decode("utf-8")
     # Split the output into tuples (title, markup_text).
-    pagesplitter = re.compile(r'>>>>CATEGORY: ([\w\s]+?)<<<<(.+?)>>>>CATEGORYEND<<<<', re.DOTALL)
-    for title, markup_text in pagesplitter.findall(out) + [
-            ("index", "Choose a plugin type on the left to see its documentation.")]:
+    pagesplitter = re.compile(r">>>>CATEGORY: ([\w\s]+?)<<<<(.+?)>>>>CATEGORYEND<<<<", re.DOTALL)
+    pages = pagesplitter.findall(out)
+    titles = [title for title, _ in pages]
+    pages.extend([
+        ("index", "Choose a plugin type on the left to see its documentation.")])
+    for title, text in pages:
+        text = insert_wiki_links(text, titles)
         document = markup.Document(title="", date="")
-        document.add_text(markup_text)
+        document.add_text(text)
         output = document.render("md", options=TXT2TAGS_OPTIONS)
-        print(document.text)
         with open(f"{outdir}/{title}.md", "w") as f:
             f.write(output)
 
--- src/search/options/doc_printer.cc
+++ src/search/options/doc_printer.cc
@@ -114,8 +114,9 @@ void Txt2TagsPrinter::print_usage(const string &name, const PluginInfo &info) {
 }
 
 void Txt2TagsPrinter::print_arguments(const PluginInfo &info) {
+    // txt2tags generates broken Markdown for nested lists. Use asterisks directly.
     for (const ArgumentInfo &arg : info.arg_help) {
-        os << "- //" << arg.key << "// (" << arg.type_name;
+        os << "* //" << arg.key << "// (" << arg.type_name;
         if (arg.bounds.has_bound())
             os << " \"\"" << arg.bounds << "\"\"";
         os << "): " << arg.help << endl;
@@ -125,7 +126,7 @@ void Txt2TagsPrinter::print_arguments(const PluginInfo &info) {
                     os << endl << "```" << endl << explanation.first << endl
                        << "```" << endl << " " << explanation.second << endl;
                 } else {
-                    os << " - ``" << explanation.first << "``: "
+                    os << "    * ``" << explanation.first << "``: "
                        << explanation.second << endl;
                 }
             }
