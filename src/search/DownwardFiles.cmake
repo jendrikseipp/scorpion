bc6543780f380cf88b5cb94f5e71e0815a109d69
Jendrik Seipp <jendrik.seipp@liu.se>
2021-10-03 15:22:03 +0200

    [scp-online] Uncomment UCP and ZOCP.

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -657,10 +657,10 @@ fast_downward_plugin(
         cost_saturation/saturated_cost_partitioning_heuristic
         cost_saturation/saturated_cost_partitioning_online_heuristic
         cost_saturation/types
-        #cost_saturation/uniform_cost_partitioning_heuristic
+        cost_saturation/uniform_cost_partitioning_heuristic
         cost_saturation/unsolvability_heuristic
         cost_saturation/utils
-        #cost_saturation/zero_one_cost_partitioning_heuristic
+        cost_saturation/zero_one_cost_partitioning_heuristic
     DEPENDS CEGAR LP_SOLVER PDBS PRIORITY_QUEUES SAMPLING TASK_PROPERTIES
 )
 
