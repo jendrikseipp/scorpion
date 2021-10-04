4e5673601a2a86eb2f4f2dabdab2ad64f98e0a2d
Jendrik Seipp <jendrik.seipp@liu.se>
2021-10-04 22:31:30 +0200

    [scp-pattern-generator] Add empty lines.

--- src/search/pdbs/match_tree.h
+++ src/search/pdbs/match_tree.h
@@ -48,9 +48,11 @@ public:
     */
     void get_applicable_operator_ids(
         int state_index, std::vector<int> &operator_ids) const;
+
     const std::vector<int> &get_hash_multipliers() const {
         return hash_multipliers;
     }
+
     void dump() const;
 };
 }
