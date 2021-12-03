3be5d0282394d82408e669bbdbd6ea3219fd4aab
Jendrik Seipp <jendrik.seipp@liu.se>
2021-12-03 17:52:27 +0100

    Make timer output clearer.

--- src/search/cegar/cegar.cc
+++ src/search/cegar/cegar.cc
@@ -259,7 +259,7 @@ void CEGAR::refinement_loop() {
         }
     }
     utils::g_log << "Time for finding abstract traces: " << find_trace_timer << endl;
-    utils::g_log << "Time for finding flaws: " << find_flaw_timer << endl;
+    utils::g_log << "Time for finding flaws and computing splits: " << find_flaw_timer << endl;
     utils::g_log << "Time for splitting states: " << refine_timer << endl;
     utils::g_log << "Time for updating goal distances: " << update_goal_distances_timer << endl;
     utils::g_log << "Number of refinements: " << abstraction->get_num_states() - 1 << endl;
