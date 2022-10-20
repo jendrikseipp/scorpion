897a408d55322dd8388667bd7ef289511294099e
Dominik Drexler <dominik.drexler@liu.se>
2022-10-20 18:13:47 +0200

    added state mapper

--- src/search/novelty/state_mapper.h
+++ src/search/novelty/state_mapper.h
@@ -15,9 +15,19 @@ class StateMapper {
 private:
     std::shared_ptr<dlplan::core::VocabularyInfo> m_vocabulary_info;
     std::shared_ptr<dlplan::core::InstanceInfo> m_instance_info;
+
+    std::vector<int> m_fact_index_to_dlplan_atom_index;
 public:
-    StateMapper();
-    ~StateMapper();
+    /**
+     * Maps task proxy information to the information from the given file
+     * such that we can map State to dlplan::core::State.
+     */
+    StateMapper(
+        const TaskProxy &task_proxy,
+        const std::string& atoms_filename,
+        const std::string& predicates_filename,
+        const std::string& static_atoms_filename,
+        const std::string& goal_atoms_filename);
 
     dlplan::core::State compute_dlplan_state(const State& state) const;
 };
