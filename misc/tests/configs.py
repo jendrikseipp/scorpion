e96a996ca02f5c35b302e661af4fd44a40e92efa
Jendrik Seipp <jendrik.seipp@liu.se>
2025-02-02 14:53:50 +0100

    Fix tests.

--- misc/tests/configs.py
+++ misc/tests/configs.py
@@ -186,7 +186,7 @@ def configs_optimal_extended():
         "exhaustive": ["--search", "dump_reachable_search_space()"],
     }
     configs.update({f"astar_cegar_{tsr}": ["--search", f"astar(cegar(transition_representation={tsr}))"]
-                    for tsr in ["store", "naive", "sg", "rh", "sg_rh", "store_then_sg_rh"]})
+                    for tsr in ["store", "naive", "sg", "rh", "sg_rh"]})
     return configs
 
 
