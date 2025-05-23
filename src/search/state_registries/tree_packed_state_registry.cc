5ecd8644d5d0da98cd15c5e305c8d4194f24ab56
Oliver Harold <oliverhsj@hotmail.dk>
2025-05-23 13:50:40 +0200

    Packed tree compression now works

---
+++ src/search/state_registries/tree_packed_state_registry.cc
@@ -0,0 +1,138 @@
+#include "tree_packed_state_registry.h"
+
+#include "../per_state_information.h"
+#include "../task_proxy.h"
+
+#include "../task_utils/task_properties.h"
+#include "../utils/logging.h"
+
+#include <iostream>
+
+using namespace std;
+
+//constexpr std::vector<int> unpacked_state_variable_reader(const int index, void* context) noexcept {
+//    auto root_table = reinterpret_cast<const vs::RootIndices *>(context);
+//    std::vector<int> state_values;
+//    vs::static_tree::read_state(index, size, *root_table, state_values);
+//    return std::move();
+//}
+
+TreePackedStateRegistry::TreePackedStateRegistry(const TaskProxy &task_proxy)
+    : IStateRegistry(task_proxy), state_packer(task_properties::g_state_packers[task_proxy]),
+      axiom_evaluator(g_axiom_evaluators[task_proxy]),
+      num_variables(task_proxy.get_variables().size()) {
+
+
+    State::get_variable_value = [this](const StateID& id) {
+            std::vector<vs::Index> buffer(get_bins_per_state());
+            vs::static_tree::read_state(id.value, get_bins_per_state(), tree_table, root_table, buffer);
+
+            std::vector<int> state_data(num_variables);
+            for (int i = 0; i < num_variables; ++i) {
+                state_data[i] = state_packer.get(buffer.data(), i);
+            }
+
+            return std::vector<int>{state_data.begin(), state_data.end()};
+    };
+
+}
+
+StateID TreePackedStateRegistry::insert_id_or_pop_state() {
+    /*
+      Attempt to insert a StateID for the last state of state_data_pool
+      if none is present yet. If this fails (another entry for this state
+      is present), we have to remove the duplicate entry from the
+      state data pool.
+    */
+//    StateID id(state_data_pool.size() - 1);
+//    auto result = registered_states.insert(id.value);
+//    bool is_new_entry = result.second;
+//    if (!is_new_entry) {
+//        state_data_pool.pop_back();
+//    }
+//    assert(registered_states.size() == state_data_pool.size());
+//    return StateID(*result.first);
+    return StateID(0);
+}
+
+State TreePackedStateRegistry::lookup_state(StateID id) const {
+    std::vector<vs::Index> tmp(get_bins_per_state());
+    vs::static_tree::read_state(id.value, get_bins_per_state(), tree_table, root_table, tmp);
+    std::vector<int> state_values{tmp.begin(), tmp.end()};
+
+    return task_proxy.create_state(*this, id, move(state_values));
+}
+
+State TreePackedStateRegistry::lookup_state(
+    StateID id, vector<int> &&state_values) const {
+    return lookup_state(id);
+}
+
+const State &TreePackedStateRegistry::get_initial_state() {
+    if (!cached_initial_state) {
+        State initial_state = task_proxy.get_initial_state();
+
+
+        std::vector<PackedStateBin> buffer(get_bins_per_state());
+        auto &tmp = initial_state.get_unpacked_values();
+        for (auto i = 0; i < num_variables; ++i) {
+            state_packer.set(buffer.data(), i, tmp[i]);
+        }
+        auto [index, _] = vs::static_tree::insert(std::vector<vs::Index>{buffer.begin(), buffer.end()},
+                                                  tree_table, root_table);
+        StateID id = StateID(index);
+        cached_initial_state = make_unique<State>(lookup_state(id));
+
+        cached_initial_state->unpack();
+    }
+    return *cached_initial_state;
+}
+
+//TODO it would be nice to move the actual state creation (and operator application)
+//     out of the PackedStateRegistry. This could for example be done by global functions
+//     operating on state buffers (unsigned *).
+State TreePackedStateRegistry::get_successor_state(const State &predecessor, const OperatorProxy &op) {
+    assert(!op.is_axiom());
+
+    std::vector<unsigned> state_values;
+
+    predecessor.unpack();
+    auto& tmp = predecessor.get_unpacked_values();
+    std::vector<vs::Index> new_state_values(tmp.begin(), tmp.end());
+
+    /* Experiments for issue348 showed that for tasks with axioms it's faster
+       to compute successor states using unpacked data. */
+    for (EffectProxy effect : op.get_effects()) {
+        if (does_fire(effect, predecessor)) {
+            FactPair effect_pair = effect.get_fact().get_pair();
+            new_state_values[effect_pair.var] = effect_pair.value;
+        }
+    }
+
+    std::vector<PackedStateBin> buffer(get_bins_per_state());
+    for (auto i = 0; i < num_variables; ++i) {
+        state_packer.set(buffer.data(), i, new_state_values[i]);
+    }
+    auto [index, inserted] = vs::static_tree::insert(buffer, tree_table, root_table);
+
+    if (!inserted) {
+        auto it = std::find(root_table.begin(), root_table.end(), index);
+        return lookup_state(StateID(it - root_table.begin()));
+    }
+
+    return lookup_state(StateID(index), {new_state_values.begin(), new_state_values.end()});
+}
+
+int TreePackedStateRegistry::get_state_size_in_bytes() const {
+    return num_variables * sizeof(unsigned);
+}
+
+int TreePackedStateRegistry::get_bins_per_state() const {
+    return state_packer.get_num_bins();
+}
+void TreePackedStateRegistry::print_statistics(utils::LogProxy &log) const {
+
+    log << "Number of registered states: " << size() << endl;
+    log << "Closed list load factor: " << root_table.size()
+        << "/" << root_table.capacity() << endl;
+}
---
+++ src/search/state_registries/tree_packed_state_registry.h
@@ -0,0 +1,251 @@
+#ifndef TREE_PACKED_STATE_REGISTRY_H
+#define TREE_PACKED_STATE_REGISTRY_H
+
+#include "../abstract_task.h"
+#include "../axioms.h"
+#include "../state_id.h"
+#include "../state_registry.h"
+
+#include "../algorithms/int_packer.h"
+#include "../algorithms/segmented_vector.h"
+#include "../algorithms/subscriber.h"
+#include "../utils/hash.h"
+
+#include <valla/declarations.hpp>
+#include <valla/static_tree_compression.hpp>
+
+#include <parallel_hashmap/phmap.h>
+
+#include <set>
+
+/*
+  Overview of classes relevant to storing and working with registered states.
+
+  State
+    Objects of this class can represent registered or unregistered states.
+    Registered states contain a pointer to the TreePackedStateRegistry that created them
+    and the ID they have there. Using this data, states can be used to index
+    PerStateInformation objects.
+    In addition, registered states have a pointer to the packed data of a state
+    that is stored in their registry. Values of the state can be accessed
+    through this pointer. For situations where a state's values have to be
+    accessed a lot, the state's data can be unpacked. The unpacked data is
+    stored in a vector<int> to which the state maintains a shared pointer.
+    Unregistered states contain only this unpacked data. Compared to registered
+    states, they are not guaranteed to be reachable and use no form of duplicate
+    detection.
+    Copying states is relatively cheap because the actual data does not have to
+    be copied.
+
+  StateID
+    StateIDs identify states within a state registry.
+    If the registry is known, the ID is sufficient to look up the state, which
+    is why IDs are intended for long term storage (e.g. in open lists).
+    Internally, a StateID is just an integer, so it is cheap to store and copy.
+
+  -------------
+
+  TreePackedStateRegistry
+    The TreePackedStateRegistry allows to create states giving them an ID. IDs from
+    different state registries must not be mixed.
+    The TreePackedStateRegistry also stores the actual state data in a memory friendly way.
+    It uses the following class:
+
+  SegmentedArrayVector<std::vector<int>>
+    This class is used to store the actual (packed) state data for all states
+    while avoiding dynamically allocating each state individually.
+    The index within this vector corresponds to the ID of the state.
+
+  PerStateInformation<T>
+    Associates a value of type T with every state in a given TreePackedStateRegistry.
+    Can be thought of as a very compactly implemented map from State to T.
+    References stay valid as long as the state registry exists. Memory usage is
+    essentially the same as a vector<T> whose size is the number of states in
+    the registry.
+
+
+  ---------------
+  Usage example 1
+  ---------------
+  Problem:
+    A search node contains a state together with some information about how this
+    state was reached and the status of the node. The state data is already
+    stored and should not be duplicated. Open lists should in theory store search
+    nodes but we want to keep the amount of data stored in the open list to a
+    minimum.
+
+  Solution:
+
+    SearchNodeInfo
+      Remaining part of a search node besides the state that needs to be stored.
+
+    SearchNode
+      A SearchNode combines a StateID, a reference to a SearchNodeInfo and
+      OperatorCost. It is generated for easier access and not intended for long
+      term storage. The state data is only stored once an can be accessed
+      through the StateID.
+
+    SearchSpace
+      The SearchSpace uses PerStateInformation<SearchNodeInfo> to map StateIDs to
+      SearchNodeInfos. The open lists only have to store StateIDs which can be
+      used to look up a search node in the SearchSpace on demand.
+
+  ---------------
+  Usage example 2
+  ---------------
+  Problem:
+    In the landmark heuristics each state should store which landmarks are
+    already reached when this state is reached. This should only require
+    additional memory when these heuristics are used.
+
+  Solution:
+    The heuristic object uses an attribute of type PerStateBitset to store for each
+    state and each landmark whether it was reached in this state.
+*/
+
+namespace vs = valla;
+namespace utils {
+class LogProxy;
+}
+
+
+using IStateRegistry = StateRegistry;
+class TreePackedStateRegistry :
+    public IStateRegistry {
+
+    vs::IndexedHashSet tree_table = vs::IndexedHashSet();
+    vs::RootIndices root_table    = vs::RootIndices();
+
+    const int_packer::IntPacker &state_packer;
+    AxiomEvaluator &axiom_evaluator;
+    const int num_variables;
+
+    std::unique_ptr<State> cached_initial_state;
+
+
+    StateID insert_id_or_pop_state();
+    int get_bins_per_state() const;
+
+public:
+    explicit TreePackedStateRegistry(const TaskProxy &task_proxy);
+
+    const TaskProxy &get_task_proxy() const override {
+        return task_proxy;
+    }
+
+    int get_num_variables() const override {
+        return num_variables;
+    }
+
+    const int_packer::IntPacker &get_state_packer() const override {
+        return state_packer;
+    }
+
+    /*
+      Returns the state that was registered at the given ID. The ID must refer
+      to a state in this registry. Do not mix IDs from from different registries.
+    */
+    State lookup_state(StateID id) const override;
+
+    /*
+      Like lookup_state above, but creates a state with unpacked data,
+      moved in via state_values. It is the caller's responsibility that
+      the unpacked data matches the state's data.
+    */
+    State lookup_state(StateID id, std::vector<int> &&state_values) const override;
+
+    /*
+      Returns a reference to the initial state and registers it if this was not
+      done before. The result is cached internally so subsequent calls are cheap.
+    */
+    const State &get_initial_state() override;
+
+    /*
+      Returns the state that results from applying op to predecessor and
+      registers it if this was not done before. This is an expensive operation
+      as it includes duplicate checking.
+    */
+    State get_successor_state(const State &predecessor, const OperatorProxy &op) override;
+
+    /*
+      Returns the number of states registered so far.
+    */
+    size_t size() const override {
+        return root_table.size();
+    }
+
+    int get_state_size_in_bytes() const;
+
+    void print_statistics(utils::LogProxy &log) const override;
+
+    class const_iterator {
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = StateID;
+        using difference_type = ptrdiff_t;
+        using pointer = StateID *;
+        using reference = StateID &;
+
+        /*
+          We intentionally omit parts of the forward iterator concept
+          (e.g. default construction, copy assignment, post-increment)
+          to reduce boilerplate. Supported compilers may complain about
+          this, in which case we will add the missing methods.
+        */
+
+        friend class TreePackedStateRegistry;
+        const TreePackedStateRegistry &registry;
+        StateID pos;
+
+        const_iterator(const TreePackedStateRegistry &registry, size_t start)
+            : registry(registry), pos(start) {
+            utils::unused_variable(this->registry);
+        }
+public:
+        const_iterator &operator++() {
+            ++pos.value;
+            return *this;
+        }
+
+        bool operator==(const const_iterator &rhs) const {
+            assert(&registry == &rhs.registry);
+            return pos == rhs.pos;
+        }
+
+        bool operator!=(const const_iterator &rhs) const {
+            return !(*this == rhs);
+        }
+
+        StateID operator*() {
+            return pos;
+        }
+
+        StateID *operator->() {
+            return &pos;
+        }
+    };
+    class iterator_impl : public IStateRegistry::const_iterator {
+        const TreePackedStateRegistry *registry_;
+        size_t idx_;
+    public:
+        iterator_impl(const TreePackedStateRegistry *reg, size_t i) : registry_(reg), idx_(i) {}
+        StateID operator*() const override { return StateID(idx_); }
+        const_iterator &operator++() override { ++idx_; return *this; }
+        bool operator==(const const_iterator &other) const override {
+            auto p = dynamic_cast<const iterator_impl*>(&other);
+            return p && registry_ == p->registry_ && idx_ == p->idx_;
+        }
+        std::unique_ptr<const_iterator> clone() const override {
+            return std::make_unique<iterator_impl>(registry_, idx_);
+        }
+    };
+
+    std::unique_ptr<IStateRegistry::const_iterator> begin() const override {
+        return std::make_unique<iterator_impl>(this, 0);
+    }
+    std::unique_ptr<IStateRegistry::const_iterator> end() const override {
+        return std::make_unique<iterator_impl>(this, size());
+    }
+
+};
+
+#endif
